0b4b782dd97c9e9520d80098214a2b0bdcb58e44
ChristianCB16 <00149618@uca.edu.sv>
2019-11-23 12:39:42 -0600

    ayudita

---
+++ index.js
@@ -0,0 +1,69 @@
+function Vista_Lunes() {
+    var element = document.getElementById("Dia1");
+    var element_add0 = document.getElementById("ACERCA");
+    var element_add1 = document.getElementById("Dia2");
+    var element_add2 = document.getElementById("Dia3");
+    var element_add3 = document.getElementById("Dia4");
+    element.classList.remove("hide");
+    element_add0.classList.add("hide");
+    element_add1.classList.add("hide");
+    element_add2.classList.add("hide");
+    element_add3.classList.add("hide");
+  }
+  
+  
+  function Vista_Martes() {
+      var element = document.getElementById("Dia2");
+      var element_add0 = document.getElementById("ACERCA");
+      var element_add1 = document.getElementById("Dia1");
+      var element_add2 = document.getElementById("Dia3");
+      var element_add3 = document.getElementById("Dia4");
+      element.classList.remove("hide");
+      element_add0.classList.add("hide");
+      element_add1.classList.add("hide");
+      element_add2.classList.add("hide");
+      element_add3.classList.add("hide");
+    }
+  
+  
+  function Vista_Miercoles() {
+      var element = document.getElementById("Dia3");
+      var element_add0 = document.getElementById("ACERCA");
+      var element_add1 = document.getElementById("Dia1");
+      var element_add2 = document.getElementById("Dia2");
+      var element_add3 = document.getElementById("Dia4");
+      element.classList.remove("hide");
+      element_add0.classList.add("hide");
+      element_add1.classList.add("hide");
+      element_add2.classList.add("hide");
+      element_add3.classList.add("hide");
+    }
+  
+  
+  function Vista_Jueves() {
+      var element = document.getElementById("Dia4");
+      var element_add0 = document.getElementById("ACERCA");
+      var element_add1 = document.getElementById("Dia1");
+      var element_add2 = document.getElementById("Dia2");
+      var element_add3 = document.getElementById("Dia3");
+  
+      element.classList.remove("hide");
+      element_add0.classList.add("hide");
+      element_add1.classList.add("hide");
+      element_add2.classList.add("hide");
+      element_add3.classList.add("hide");
+    }
+  
+  
+  function Vista_ACERCA() {
+      var element = document.getElementById("ACERCA");
+      var element_add0 = document.getElementById("Dia4");
+      var element_add1 = document.getElementById("Dia1");
+      var element_add2 = document.getElementById("Dia2");
+      var element_add3 = document.getElementById("Dia3");
+      element.classList.remove("hide");
+      element_add0.classList.add("hide");
+      element_add1.classList.add("hide");
+      element_add2.classList.add("hide");
+      element_add3.classList.add("hide");
+    }
